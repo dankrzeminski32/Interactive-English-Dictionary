79e90271635799e645476041546277fcfc9743db
Daniel Krzeminski <dankrzeminski32@gmail.com>
2021-04-07 16:04:58 -0500

    similar word on input mistake

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import json
-
+from difflib import SequenceMatcher, get_close_matches
 
 data = json.load(open("data.json"))
 
 def definition(word):
-    return data[word]
+    word = word.lower()
+    if desired_word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+
+         feedback = input("Did you mean %s instead? (Y/N) " % get_close_matches(word, data.keys())[0])
+         if feedback == 'Y' or feedback == 'y':
+             return data[get_close_matches(word, data.keys())[0]]
+         else:
+             return "Sorry your original word does not exist."
+    else:
+        return "Sorry that word does not exist in our database."
 
 
 desired_word = input("Which word would you like the definition to: ")
 
-desired_word = desired_word.lower()
+print(definition(desired_word))
 
-if desired_word in data:
-    print(definition(desired_word))
-else:
-    print("Sorry that word does not exist in our database.")
